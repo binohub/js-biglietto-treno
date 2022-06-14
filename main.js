c0c59d75a1235d447af0de079d1d24752c82abd1
binohub <yeebino001@gmail.com>
2022-06-14 15:06:29 +0200

    dudeAge - done

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 
 // variabili globali
-const ticketPrice = 0.21
-console.log(`oggi il prezzo è di soli ${ticketPrice} a km! `)
+const ticketPrice = 0.21;
+console.log(`oggi il prezzo è di soli ${ticketPrice} a km! `);
+
+
+
 
 //chiedi all'utente il numero di km che vuole percorrere = dudeKm
 const dudeKm = prompt("quanti km vuoi percorrere?");
 console.log(`vuoi percorrere ${dudeKm} km`);
 
-//! chiedi all'utente l'età = dudeAge
-    //! se minore di 18
-        //! sconto del 20
-        //? dudeSale = (ticketFullPrice * 20) / 100
-    //! se invece maggiore di 65
-        //! sconto del 40
-        //? dudeSale = (ticketFullPrice * 40) / 100
-    //! altrimenti
-        //! nessuno sconto per te :c
-        //? dudeSale = 0
+//stampa il ticketFullPrice
+const ticketFullPrice = ticketPrice * dudeKm;
+console.log(`il prezzo totale è di ${ticketFullPrice}€`);
 
-        
+//chiedi all'utente l'età e applica lo sconto
+const dudeAge = prompt("se digiti la tua età posso controllare per te se puoi usufruire di qualche sconto speciale!");
+//se età minore di 18
+if (dudeAge < 18){
+    const dudeSale = parseInt((ticketFullPrice * 20) / 100);
+    console.log(`per i minorenni abbiamo uno sconto del 20% ovvero ${dudeSale} `);
+}else if (dudeAge < 65){
+    const dudeSale = (ticketFullPrice * 40) / 100;
+    console.log(`per gli over 65 abbiamo uno sconto del 40%`);
+}else{
+    console.log(`non abbiamo sconti per te :c`);
+}
 
-        
-
-        
-
-//! calcola e stampa il ticketFullPrice
-// const ticketFullPrice = ticketPrice * dudeKm
-// console.log(`il prezzo totale è di ${ticketFullPrice}€`);
-
-//! calcola e stampa il ticketFinalPrice
-// const ticketFinalPrice = ticketFullPrice - dudeSale
+//stampa il ticketFinalPrice
+const ticketFinalPrice = ticketFullPrice - dudeSale;
+console.log(`il prezzo finale è di ${ticketFinalPrice}€`);
 
