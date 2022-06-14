e9d5e5625bd8d0c5e3f949b00cf2e837005e194f
binohub <yeebino001@gmail.com>
2022-06-14 14:11:08 +0200

    start

---
+++ main.js
@@ -0,0 +1,18 @@
+// ! VARIABILI GLOBALI
+    // ! ticketPrice = 0.21
+    // ! ticketFullPrice = Ticketprice * dudeKm
+    // todo ticketFinalPrice = ticketFullPrice - dudeSale
+
+// ! CHIEDI all'utente il NUMERO di KM che vuole percorrere =dudeKm
+
+// ! CHIEDI all'utente l'ETA' = dudeAge
+    // ! SE <= 18
+        //? dudeSale = (ticketFullPrice * 20) / 100
+    // ! SE INVECE > 65
+        //? dudeSale = (ticketFullPrice * 40) / 100
+    // ! ALTRIMENTI
+        //? dudeSale = 0
+        
+// ! STAMPIAMO a video il ticketFullPrice
+
+// ! STAMPIAMO a video il ticketFinalPrice
