768aa43d6a4acf29e39911c8e8560b563f84578f
binohub <yeebino001@gmail.com>
2022-06-14 14:36:55 +0200

    dudeKm - done

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
-// ! VARIABILI GLOBALI
-    // ! ticketPrice = 0.21
-    // ! ticketFullPrice = Ticketprice * dudeKm
-    // todo ticketFinalPrice = ticketFullPrice - dudeSale
 
-// ! CHIEDI all'utente il NUMERO di KM che vuole percorrere =dudeKm
+// variabili globali
+const ticketPrice = 0.21
+console.log(`oggi il prezzo è di soli ${ticketPrice} a km! `)
 
-// ! CHIEDI all'utente l'ETA' = dudeAge
-    // ! SE <= 18
+//chiedi all'utente il numero di km che vuole percorrere = dudeKm
+const dudeKm = prompt("quanti km vuoi percorrere?");
+console.log(`vuoi percorrere ${dudeKm} km`);
+
+//! chiedi all'utente l'età = dudeAge
+    //! se minore di 18
+        //! sconto del 20
         //? dudeSale = (ticketFullPrice * 20) / 100
-    // ! SE INVECE > 65
+    //! se invece maggiore di 65
+        //! sconto del 40
         //? dudeSale = (ticketFullPrice * 40) / 100
-    // ! ALTRIMENTI
+    //! altrimenti
+        //! nessuno sconto per te :c
         //? dudeSale = 0
-        
-// ! STAMPIAMO a video il ticketFullPrice
 
-// ! STAMPIAMO a video il ticketFinalPrice
+        
+
+        
+
+        
+
+//! calcola e stampa il ticketFullPrice
+// const ticketFullPrice = ticketPrice * dudeKm
+// console.log(`il prezzo totale è di ${ticketFullPrice}€`);
+
+//! calcola e stampa il ticketFinalPrice
+// const ticketFinalPrice = ticketFullPrice - dudeSale
+
